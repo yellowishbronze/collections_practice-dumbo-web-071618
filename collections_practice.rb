3c5f0a8bc44877d5c374476e0ed6c7f6cc15324b
Bryan Teng <bteng1226@gmail.com>
2018-07-19 13:42:23 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
+def sort_array_asc(array)
+  return array.sort
+end
 
+def sort_array_desc(array)
+  return array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by{|word| word.length}
+end
+
+def swap_elements(array)
+word1= array[1]
+word2=array[2]
+
+array[1] = word2
+array[2] = word1
+return array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each{|word| word[2] = "$"}
+end
+
+def find_a(array)
+  array.keep_if{|word| word[0] == "a"}
+end
+
+def sum_array(array)
+  array.reduce(:+)
+end
+
+def add_s(array)
+  array.each_with_index.map{|word,index| word+"s" unless index == 1}
+end
